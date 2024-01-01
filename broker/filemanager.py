261e2304ffab14582c3065d280756468520366a1
Alireza Alipanah <alipanah.alireza@gmail.com>
2024-01-01 17:51:29 +0330

    bug fixes

--- broker/filemanager.py
+++ broker/filemanager.py
@@ -7,13 +7,15 @@ class FileManager:
 
     def __init__(self):
         self.log_idx = 0 # index of last log file created
-        self.last_log = f'{logs/self.log_idx}.log' # path of last log file
+        self.log_dir = 'logs'
+        os.makedirs(self.log_dir, exist_ok=True)
+        self.last_log = os.path.join(self.log_dir, f'{self.log_idx}.log') # path of last log file
         with open(self.last_log, 'w') as log_file:
             log_file.write('')
 
     def read(self):
         try:
-            with open('yourfile.txt', 'r+') as log_file:
+            with open(self.last_log, 'r+') as log_file:
                 lines = log_file.readlines()
                 log_file.seek(0)
                 log_file.truncate()
@@ -26,10 +28,27 @@ class FileManager:
         
 
     def write(self, obj):
-        object_json = json.dumps(obj)
+        object_json = json.dumps(obj, default=vars)
         try:
             with open(self.last_log, 'a') as log_file:
                 log_file.write(f'\n{object_json}')
         except Exception as e:
             print(f'unable to write due to exception:\n{e}')
             
+
+if __name__ == '__main__':
+    filemanager = FileManager()
+    print(filemanager.read())
+
+    class DummyClass:
+        def __init__(self, var1, var2):
+            self.var1 = var1
+            self.var2 = var2
+            
+    filemanager.write(DummyClass(1, 2))
+    print(filemanager.read())
+    filemanager.write(DummyClass(3, 4))
+    filemanager.write(DummyClass(5, 6))
+    print(filemanager.read())
+    print(filemanager.read())
+
