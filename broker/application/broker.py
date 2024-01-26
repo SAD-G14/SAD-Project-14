d40dcffe0ddb4129d5448e38f8d2c452baae72f6
ASUS <pro.alir.pro@gmail.com>
2024-01-26 19:01:45 +0330

    add server idempotency

--- broker/application/broker.py
+++ broker/application/broker.py
@@ -8,8 +8,12 @@ db = DB()
 def push(message_data: MessageData) -> dict:
     message = MessageModel.from_data(message_data)
     written_message = db.write(message)
-    #return written_message
-    return {'producer_id': written_message.producer_id, 'sequence_number': written_message.sequence_number}
+    # return written_message
+    if written_message:
+        return {'producer_id': written_message.producer_id, 'sequence_number': written_message.sequence_number}
+    else:
+        return {'status': 'duplicate message'""", 'producer_id': message_data.producer_id,
+                'sequence_number': message_data.sequence_number"""}
 
 
 def pull():
--- broker/application/interfaces.py
+++ broker/application/interfaces.py
@@ -2,9 +2,23 @@ from broker.model.message import Message
 
 
 class DB:
+    def __init__(self):
+        self.last_sequence_numbers = {}
+
     # this should also set the sequence number
     def write(self, message: Message) -> Message:
-        pass
+        if self.is_duplicate(message.producer_id, message.sequence_number):
+            return None
+        # Write the message to the DB
+
+        self.update_sequence_number(message.producer_id, message.sequence_number)
+        return message
 
     def read(self) -> Message:
         pass
+
+    def update_sequence_number(self, producer_id, sequence_number):
+        self.last_sequence_numbers[producer_id] = sequence_number
+
+    def is_duplicate(self, producer_id, sequence_number):
+        return self.last_sequence_numbers.get(producer_id, -1) >= sequence_number
