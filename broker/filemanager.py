3673d62f90097b8eefd31417eaff0bcff09a6df5
Alireza Alipanah <alipanah.alireza@gmail.com>
2024-02-09 10:35:41 +0330

    added find message in queue

--- broker/filemanager.py
+++ broker/filemanager.py
@@ -23,6 +23,7 @@ class FileManager:
                 # todo: unused variable: last_line = log_file.writelines(lines[:-1])
         except Exception as e:
             print(f'unable to read from file due to exception:\n{e}')
+            return None
         if len(lines) == 0:
             return None
         return json.loads(lines[-1])
@@ -31,20 +32,29 @@ class FileManager:
         object_json = json.dumps(obj, default=vars)
         try:
             with open(self.last_log, 'a') as log_file:
-                log_file.write(f'\n{object_json}')
+                log_file.write(f'{object_json}\n')
             self.pushed_objects_count += 1
             return obj
         except Exception as e:
             print(f'unable to write due to exception:\n{e}')
 
     def find_message_in_queue(self, producer_id, sequence_number):
-        # todo: fill this method (alireza)
-        pass
+        try:
+            with open(self.last_log, 'r') as log_file:
+                lines = log_file.readlines()
+                # todo: unused variable: last_line = log_file.writelines(lines[:-1])
+        except Exception as e:
+            print(f'unable to read from file due to exception:\n{e}')
+            return None
+        logs_json = list(map(lambda x: json.loads(x), lines))
+        result = list(filter(lambda x: x['producer_id'] == producer_id and x['sequence_number'] == sequence_number,
+         logs_json))
+        return result
 
 # todo: move this to the tests
 
 
-def test_case():
+def filemanager_read_write_test_case():
     filemanager = FileManager()
     print(filemanager.read())
 
@@ -59,3 +69,31 @@ def test_case():
     filemanager.write(DummyClass(5, 6))
     print(filemanager.read())
     print(filemanager.read())
+
+
+def filemanager_find_message_in_queue_test_case():
+    class Message():
+        def __init__(self, producer_id, sequence_number):
+            self.producer_id = producer_id
+            self.sequence_number = sequence_number
+
+        def serialize(self):
+            return {
+            'producer_id': self.producer_id,
+            'sequence_number': self.sequence_number
+            }
+
+    filemanager = FileManager()
+    message = Message(1, 1)
+    filemanager.write(message)
+    filemanager.write(message)
+    message.producer_id = 2
+    filemanager.write(message)
+    message.sequence_number = 2
+    filemanager.write(message)
+    message.producer_id = 1
+    filemanager.write(message)
+    print(filemanager.find_message_in_queue(1, 1))
+
+if __name__ == '__main__':
+    filemanager_find_message_in_queue_test_case()
