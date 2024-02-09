e04b63d37a9608b7f04bb10e6d5b4041f04d6d1b
Alireza Alipanah <alipanah.alireza@gmail.com>
2024-02-09 12:59:16 +0330

    added Tests to broker/test

--- broker/test/test.py
+++ broker/test/test.py
@@ -13,5 +13,25 @@ class TestClient(unittest.TestCase):
         self.assertTrue(True)
 
 
+    def test_push_and_pull(self):
+        message_request = message.MessageRequest(key='test1', value='test2', date=1707058229,
+                                                 producer_id=1707058229693, sequence_number=1)
+        written_message = broker.push(message_request)
+        pulled_message = broker.pull()
+        self.assertEqual(message_request.serialize(), pulled_message)
+
+    def test_ack_successful(self):
+        message_request = message.MessageRequest(key='test1', value='test2', date=1707058229,
+                                                 producer_id=1707058229693, sequence_number=1)
+        written_message = broker.push(message_request)
+        self.assertTrue(broker.ack(1707058229693, 1))
+
+    def test_ack_unsuccessful(self):
+        message_request = message.MessageRequest(key='test1', value='test2', date=1707058229,
+                                                 producer_id=1707058229693, sequence_number=1)
+        written_message = broker.push(message_request)
+        self.assertTrue(broker.ack(1707058229693, 2))
+
+
 if __name__ == '__main__':
     unittest.main()
