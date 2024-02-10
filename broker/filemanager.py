0098b4a0084dd624ff9bad548cfeadcdaf03a31e
shahab <shhm3834@gmail.com>
2024-02-11 02:05:16 +0330

    Fix bugs (client host and file name)

--- broker/filemanager.py
+++ broker/filemanager.py
@@ -4,12 +4,12 @@ import json
 
 class FileManager:
 
-    def __init__(self):
+    def __init__(self, partition, replica):
         self.pushed_objects_count = 0
         self.log_idx = 0  # index of last log file created
         self.log_dir = 'logs'
         os.makedirs(self.log_dir, exist_ok=True)
-        self.last_log = os.path.join(self.log_dir, f'{self.log_idx}.log')  # path of last log file
+        self.last_log = os.path.join(self.log_dir, f'{self.log_idx}-{partition}-{replica}.log')  # path of last log file
         with open(self.last_log, 'w') as log_file:
             log_file.write('')
 
